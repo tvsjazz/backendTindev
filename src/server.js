d7cfc2489694004363f48b0fcf9c9b8716134223
Tiago Siqueira <tvsjazz@gmail.com>
2019-08-07 09:19:30 -0300

    add bd e mongoose

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const server = express();
+
+mongoose.connect('mongodb+srv://tvsjazz:1Secret9@cluster0-rcmvp.mongodb.net/omnistack8?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
 
 server.use(express.json());
 server.use(routes);
