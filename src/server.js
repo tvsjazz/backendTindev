6bb6558bcad1fca1748da80f8697813bbc8e5936
Tiago Siqueira <tvsjazz@gmail.com>
2019-08-09 09:26:31 -0300

    backend finished

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
 
 const server = express();
@@ -8,6 +10,7 @@ mongoose.connect('mongodb+srv://tvsjazz:1Secret9@cluster0-rcmvp.mongodb.net/omni
     useNewUrlParser: true
 });
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
